986bfcb8c074fe9ce424b2d60676221e067825f2
pranavpurankar <pranav.purankar@gmail.com>
2024-09-02 14:25:25 +0530

    class and instance of

---
+++ 06_OOP_and _classes/05_inheritance.js
@@ -0,0 +1,33 @@
+class User{
+    constructor(username){
+        this.username = username;
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);    
+    }
+}
+
+const chai = new Teacher("chai", "chai@email.com", "123");
+chai.addCourse();
+
+const masalaChai = new User("masala Chai");
+
+masalaChai.logMe();
+
+chai.logMe();
+
+console.log(chai instanceof User);
