ec6d500ef6f6dcc7640924fa939fcacc37eed75c
pranavpurankar <pranav.purankar@gmail.com>
2024-11-14 02:28:26 +0530

    Revision before DSA

---
+++ Revision_DefinitiveGuide/0.fundamentals.js
@@ -0,0 +1,21 @@
+let x;
+
+x = 0;
+console.log(typeof(x));
+
+// Primitive types
+// Javascript supports several types of values their -> return types
+// Number	-> "number"
+// Strings 	-> "string"
+// Boolean 	-> "boolean"
+// Null 	-> "object"
+// Undefined 	-> "undefined"
+// BigInt	-> "bigint"
+// Symbol	-> "symbol"
+
+// Non-primitive data types
+// Object -> 
+
+let book = {
+	ty
+}
