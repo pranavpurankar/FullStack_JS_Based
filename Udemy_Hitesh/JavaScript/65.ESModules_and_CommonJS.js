dc47dc4b755e33a6c8dc6e8fcebad5f420e925c9
pranavpurankar <pranav.purankar@gmail.com>
2024-12-10 16:45:57 +0530

    AsyncChatGpt and ES Modules files updated

---
+++ Udemy_Hitesh/JavaScript/65.ESModules_and_CommonJS.js
@@ -0,0 +1,38 @@
+
+// Relearn if needed other can be learned this on the go, not too complex
+
+/* 
+[Revise this topic, getting errors while try to run the file]
+
+Modules are a technique heavily used in today's software design/
+architecture.
+
+# What are modules and why are they useful?
+A module is just a piece of code in a file that you can call and use
+from other files. A modular design is the opposite of having all your
+project's code in one single file.
+
+*/
+
+/* 
+# ES Modules
+
+It is a standard that was introduced with ES6. The idea was to standari-
+ze hos JS modules works and implement this features in browsers (which)
+didn't previously support modules.
+
+ESmodules is a more modern approach that is currently supported by
+browsers and server-side apps with Node.
+
+*/
+
+
+/* 
+# Common JS :-
+It is mainly used in server-side JS apps with Node, browser don't 
+support the use of Common JS
+
+Node used to only support Common JS to implement modules, but nowadays
+it also supports ESmodules (a modern approach)
+
+*/
